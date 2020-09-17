c5af94ffb6626c30f6697c94ec4db553a4d1a030
daniaramburu <aramburudf@gmail.com>
2020-09-17 15:19:21 -0300

    add hamburguer and nav

---
+++ index.js
@@ -0,0 +1,15 @@
+//Selecciono:
+const cruz = document.querySelector(".hamburger")
+
+//Función:
+
+cruz.onclick = () => {
+    cruz.classList.toggle(".is-active")
+
+const menu = document.querySelector(".hamburer-open")
+
+menu.classList.toggle"is_active"
+}
+
+// document.querySelector( ".menuppal" ).classList.toggle("is_active");
+// event.preventDefault();
